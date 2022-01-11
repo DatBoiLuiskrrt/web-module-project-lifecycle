b64fe8e6f3f122f9cc8dfd063432f3eedbdbe0a6
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-01-11 14:30:33 -0600

    got user to work with the api and display data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-
+import axios from "axios";
+import React from "react";
+import User from "./components/User";
 class App extends React.Component {
+  state = {
+    user: [],
+  };
+
+  componentDidMount() {
+    axios.get("https://api.github.com/users/datboiluiskrrt").then((resp) => {
+      console.log(resp.data);
+      this.setState({
+        ...this.state,
+        user: resp.data,
+      });
+    });
+  }
+
   render() {
-    return(<div>
-      Github Card
-    </div>);
+    return (
+      <div>
+        <div>Github Card</div>
+        <form>
+          <input />
+          <button>Fetch User</button>
+        </form>
+        <User data={this.state.user} />
+      </div>
+    );
   }
 }
 
---
+++ src/components/User.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class User extends React.Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <div>
+        <img src={data.avatar_url} />
+        <h1>{data.name}</h1>
+        <p>{data.login}</p>
+        <p>Total Repos: {data.public_repos}</p>
+        <p>Total Followers: {data.followers}</p>
+      </div>
+    );
+  }
+}
+
+export default User;
